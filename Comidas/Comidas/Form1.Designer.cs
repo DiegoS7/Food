4207370939cb701fd319cc50af8fe75fc445586e
dsandoval7@ucol.mx <dsandoval7@ucol.mx>
2019-09-08 22:11:21 -0500

    mmm

---
+++ Comidas/Comidas/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Comidas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.alimentos = new System.Windows.Forms.ComboBox();
+            this.Añadir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // alimentos
+            // 
+            this.alimentos.FormattingEnabled = true;
+            this.alimentos.Location = new System.Drawing.Point(15, 12);
+            this.alimentos.Name = "alimentos";
+            this.alimentos.Size = new System.Drawing.Size(209, 21);
+            this.alimentos.TabIndex = 1;
+            this.alimentos.SelectedIndexChanged += new System.EventHandler(this.Alimentos_SelectedIndexChanged);
+            // 
+            // Añadir
+            // 
+            this.Añadir.Location = new System.Drawing.Point(135, 126);
+            this.Añadir.Name = "Añadir";
+            this.Añadir.Size = new System.Drawing.Size(89, 23);
+            this.Añadir.TabIndex = 2;
+            this.Añadir.Text = "Agregar";
+            this.Añadir.UseVisualStyleBackColor = true;
+            this.Añadir.Click += new System.EventHandler(this.Añadir_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 242);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Añadir);
+            this.Controls.Add(this.alimentos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox alimentos;
+        private System.Windows.Forms.Button Añadir;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Comidas/Comidas/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comidas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            alimentos.Items.Add("Pozole");
+            alimentos.Items.Add("Tostadas de Pata");
+        }
+        private void Alimentos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = alimentos.SelectedIndex;
+            label1.Text = indice.ToString();
+            label2.Text = alimentos.Items[indice].ToString();
+        }
+
+        private void Añadir_Click(object sender, EventArgs e)
+        {
+            alimentos.Items.Add(textBox1.Text);
+        }
+
+   
+    }
+}
